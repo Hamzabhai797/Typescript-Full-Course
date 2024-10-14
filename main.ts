659251cb991ab8c006caa5c2600d7bffafd4c6cf
hamzabhai797 <hamzanoor11111122@gmail.com>
2024-10-14 04:31:50 -0700

    typescript'

---
+++ main.ts
@@ -0,0 +1,117 @@
+//variables
+
+// let myName = "hamza"
+// console.log(myName)
+// Is ma myname variable hai
+
+// data type
+//primitive datatype Humary pass 7 hoty hai
+// string
+// number
+// boolean
+// null
+// undefined
+// bigint
+// symbol
+
+// non primitive data type humary pass hoty hai
+// object  
+//object ka ander hi humary pass (Arrays and functions aaty hai)
+
+
+// operators
+// Arithmetics operator
+//+  -  *  /
+// let a = 5
+// let b = 3
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// Increment operator  ++
+// decrement opertor   --
+
+
+// Assignment operators
+// =  +=  -=  *=  /=  %=  **=
+// let a = 5 
+// let b = 2
+// a += 6
+// console.log(a)
+// a -= 6
+// console.log(a)
+// a *= 6
+// console.log(a)
+// a /= 6
+// console.log(a)
+// a %= 6
+// console.log(a)
+// a **= 6
+// console.log(a)
+
+// comparison operators
+// ==  ===  !=  !==  >  >=  <  <==
+// let a = 5
+// let b = 2
+// console.log(a == b)
+// console.log(a === b)
+// console.log(a != b)
+// console.log(a !== b)
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a <= b)
+// console.log(a >= b)
+// == operator just value check karti hay datatype nahi
+// === ya value ka saat datatype bhi check karti hay
+
+// logical operators
+// &&  ||  !
+// let a = 5
+// let b = 2
+// let cond1 = a > b
+// let cond2 = a === 5
+// console.log(cond1 && cond2)
+
+// let value = 5
+// let value2 = 3
+// let cond3 = a > b
+// let cond4 = a === 5
+// console.log(cond3 || cond4)
+
+// let a = 3
+// let b = 5
+// console.log(!(a > b) )
+
+// conditional statement
+// let age = 25;
+// if (age > 18){
+//     console.log("you can vote")
+// } else {
+//     console.log("not vote")
+// }
+
+// let age2 = 15;
+// if (age2 > 18){
+//     console.log("you can drive")
+// } else {
+//     console.log("not drive")
+// }
+
+//          practice
+// alert("Hello!")
+
+// practice Q 2
+let score = 95
+let grade
+if (score >= 90 && score <= 100){
+    grade = "A";
+} else if (score >= 70 && score <= 89){
+    grade = "B"
+} else if (score >= 50 && score <= 69){
+    grade = "C"
+} else if (score >= 40 && score <= 49){
+    grade = "D"
+} else {
+    grade = "Fail"
+}
+console.log("your grade was", grade)
